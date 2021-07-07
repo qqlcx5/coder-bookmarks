d40f093c8bd0dc3e4b6eadcc4c7255441c1f1781
qqlcx5 <qqlcx5@163.com>
2021-07-07 23:56:57 +0800

    :fire: docs

--- docs/.vuepress/config/theme.js
+++ docs/.vuepress/config/theme.js
@@ -4,6 +4,9 @@ const docsRoot = path.join(__dirname,'..', '..');
 const readFile = require(utilsRoot + '/readFile');
 
 const guide = readFile(docsRoot + '/guide');
+const interview = readFile(docsRoot + '/interview');
+const repository = readFile(docsRoot + '/repository');
+const website = readFile(docsRoot + '/website');
 
 
 
@@ -11,15 +14,31 @@ const themeConfig = {
   editLinks: true,
   smoothScroll: true,
   lastUpdated: '最后更新时间',
-  nav: [{
-        text: 'guide',
+  nav: [
+      {
+        text: '指南',
         link: '/guide/pages',
+      },
+      {
+        text: '存储库',
+        link: '/repository/pages',
+      },
+      {
+        text: '网站',
+        link: '/website/pages',
+      },
+      {
+        text: '面试',
+        link: '/interview/pages',
       }
       
   ],
   
   sidebar: {
     '/guide/': guide,
+    '/interview/': interview,
+    '/repository/': repository,
+    '/website/': website,
   },
 };
 
