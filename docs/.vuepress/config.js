96330e4e8db737496e8d4bb0014b8c2ff81b4a20
qqlcx5 <qqlcx5@163.com>
2021-07-07 23:23:56 +0800

    coder-bookmarks

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,20 +1,19 @@
 const plugins = require('./config/plugins');
 const themeConfig = require('./config/theme');
 module.exports = {
-  base: '/vuepress-template/',
-  title: 'vuepress-template',
+  base: '/coder-bookmarks/',
+  title: 'coder-bookmarks',
   description: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown',
   head: [
-    ['link', { rel: 'icon', href: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@v1.0.0/image/polar_bear.jpg' }],
+    ['link', { rel: 'icon', href: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@v1.0.0/image/star-field.png' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
     ['meta', {
         name: 'keywords',
         content: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown'},
     ],
-    ['meta', { name: 'theme-color', content: '#11a8cd' }],
-    ['script', { async: 'async', src: 'https://hm.baidu.com/hm.js?27c61fbfe7c9201c97b53d619459c89d' }],
+    ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+    ['script', { async: 'async', src: 'https://hm.baidu.com/hm.js?662dade0b3fe9911e80442e86935e233' }],
   ],
-  
   themeConfig,
   plugins
 };
--- docs/README.md
+++ docs/README.md
@@ -1,15 +1,17 @@
 ---
 home: true
-heroImage: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@v1.0.0/image/crab_logo.png'
+heroImage: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@v1.0.0/image/qc-play.png'
 heroText: 前端有道
-tagline: A magical 微信公众号
+tagline: 嗨！扫一扫上面的公众号，也是对我一种鼓励
 actionText: 点击开始 →
-actionLink: /组件/10.开发指南/01.介绍
-
+actionLink: /guide/contact
 features:
-  - title: 侧边栏分组
-    details: 根据目录自动生成侧边栏分组，避免文件名写错造成侧边栏异常的问题。
-  - title: uni-crab-site
-    details: 轻量、便捷、简洁静态网站
-footer: MIT Licensed | Copyright © 2020-present lcx
+  - title: 前端有道交流群
+    details: 有良好的学习氛围环境，解决职业中的困扰
+  - title: 汇聚优质的前端资源
+    details: 正所谓工欲善其事，必先利其器
+  - title: 技术文章分享
+    details: 尽我所能讲分享学到的知识
+
+footer: MIT Licensed | Copyright © 2020-present Evan You
 ---
